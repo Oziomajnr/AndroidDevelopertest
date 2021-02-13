9c901ab313f81d3e7a12c80173b46090e3757b46
oziomaogbe <ogbeoziomajnr@yahoo.com>
2021-02-13 17:25:18 +0100

    initial commit

---
+++ app/src/main/java/com/ozioma/dindinandroidtest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ozioma.dindinandroidtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
